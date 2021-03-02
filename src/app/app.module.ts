c677766ac1af8211637175bee90bb2b02105eeda
Alvin-Jang <jinweizh@sina.cn>
2021-03-02 11:50:25 +0800

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,28 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { CrisisListComponent } from './crisis-list/crisis-list.component';
-import { HeroesListComponent } from './heroes-list/heroes-list.component';
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
+import { CrisisListComponent } from "./crisis-list/crisis-list.component";
+import { HeroesListComponent } from "./heroes-list/heroes-list.component";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, CrisisListComponent, HeroesListComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: "crisis-list", component: CrisisListComponent },
+      { path: "heroes-list", component: HeroesListComponent }
+    ])
+  ],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    CrisisListComponent,
+    HeroesListComponent
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
