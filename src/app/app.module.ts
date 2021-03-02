dc4a85165290ca228887efb04029e647df85bd42
Alvin-Jang <jinweizh@sina.cn>
2021-03-02 11:44:38 +0800

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, CrisisListComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
